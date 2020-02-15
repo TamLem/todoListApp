30286d24988d305594544747ceaff3dedca1dc5b
TamLem <tamworku1@gmail.com>
2020-02-15 15:31:53 +0100

    list extended edit added

--- script.js
+++ script.js
@@ -28,12 +28,23 @@ function addListItem () {
 
     if (input.value!=0) {
         let userText = document.createTextNode(input.value)
-        newListItem.appendChild(userText);
-        newButton = document.createElement('button');
-        newButton.className="list-button";
-        newButton.innerText="Del";
-        
-        newListItem.appendChild(newButton);
+       
+        delButton = document.createElement('button');
+        delButton.className="list-button";
+        delButton.innerText="Del";
+
+        editButton = document.createElement('button')
+        editButton.className = "edit-button"
+        editButton.innerText = "Edit"
+
+        newspan = document.createElement('span');
+        newspan.className = 'list-text'
+        newspan.appendChild(userText);
+
+        newListItem.appendChild(delButton);
+        newListItem.appendChild(editButton);
+        newListItem.appendChild(newspan);
+
         listDiv.appendChild(newListItem)
         //console.log(newList)
         input.value = " "
@@ -43,7 +54,8 @@ function addListItem () {
 
 // Code below deletes a list item
 
-document.addEventListener('click', editListItem)
+
+document.addEventListener('click', editDelList)
 document.addEventListener('click', delListItem );
 
 function delListItem (e){
@@ -57,38 +69,54 @@ function delListItem (e){
 }
 
 
-function editListItem (e) {
-    if (e.target.className=='edit-button') {
-        var editText = document.createElement('input');
-        
-        var listItem = e.target.parentElement;
-        editText.value = listItem.querySelector('span').textContent;
-        listItem.querySelector('span').textContent = " ";
-        listItem.querySelector('span').appendChild(editText);
-        
-        let editButton = e.target.parentElement.querySelector('.edit-button');
-        
-        
-        editButton.innerText = "Done";
-        editButton.className ="done-button";
+let editBox = document.querySelector('#edit-box');
 
+//editBox.event('keydown', function(){checkEnter(event, fixEdit)});
+//match event to the click event on the button
 
-        editButton.addEventListener('click', fixEdit);
-        editText.addEventListener('keyup', function(){checkEnter(event, fixEdit)});
-
-        function fixEdit () {
-            listItem = e.target.parentElement;
-            console.log(listItem)
-            listItem.querySelector('span').removeChild(editText) ;
-            listItem.querySelector('span').textContent = editText.value;
-            console.log("event worsks")
-            editButton.innerText = "Edit";
-            editButton.className ="edit-button";
-        }
-
+function editDelList (e) { 
+    if(e.target.className=="edit-button") {
+    //if there is input text trigger done else trigger edit
+   // let editText = e.target.parentElement.querySelector('input');
+        
+    (editBox.value!="") ? fixEdit(event): editListItem(event);
     }
 }
 
+
+function editListItem (e) {
+ 
+        let editBox = document.querySelector('#edit-box');
+        let listItem = e.target.parentElement;
+        editBox.value = listItem.querySelector('span').textContent;
+        console.log(editBox);
+
+        listItem.querySelector('span').textContent="";
+        listItem.querySelector('span').appendChild(editBox);
+        editBox.style.display = 'inline';
+        console.log(listItem.querySelector('span'))
+        
+        e.target.innerText = "Done";
+    
+}
+
+function fixEdit (e) {
+    let listItem = e.target.parentElement;
+    console.log(listItem)
+
+    let editBox = document.querySelector('#edit-box')
+    console.log(editBox)
+
+    document.querySelector('body').appendChild(editBox);
+    listItem.querySelector('span').textContent = editBox.value ;
+
+    console.log("event works")
+    e.target.innerText = "Edit";
+
+    editBox.value = "";
+    editBox.style.display= 'none';
+
+}
 
 
 
